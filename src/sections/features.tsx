5de7a416fbe182409e3b3ec53f659be41fedfffe
jsina <jsina.maleki@gmail.com>
2023-05-09 16:42:30 +0330

    fix some animations

--- src/sections/features.tsx
+++ src/sections/features.tsx
@@ -13,8 +13,18 @@ export const Features = () => {
   const x = useTransform(scrollYProgress, [0.3, 1], ["50%", "0%"]);
   const opacity = useTransform(
     scrollYProgress,
-    [0, 0.5, 0.6, 0.85, 0.9],
-    [1, 1, 0.4, 0.4, 1]
+    [0, 0.5, 0.6, 0.85, 0.9, 0.91],
+    [1, 1, 0.4, 0.4, 0.1, 0]
+  );
+  const display = useTransform(
+    scrollYProgress,
+    [0, 0.5, 0.6, 0.85, 0.9, 0.91],
+    ["block", "block", "block", "block", "block", "none"]
+  );
+  const finalOpacity = useTransform(
+    scrollYProgress,
+    [0, 0.5, 0.6, 0.85, 0.9, 0.91, 0.96, 1],
+    [0, 0, 0, 0, 0, 0.9, 0.96, 1]
   );
 
   const text1Opacity = useTransform(
@@ -57,18 +67,22 @@ export const Features = () => {
     >
       <div className="sticky top-[16.7vh] h-[66.8vh] px-16 text-2xl leading-[1] text-white [&_p]:w-[45rem] [&_p]:max-w-[90%]">
         <motion.div style={{ x, scale }} className="relative h-full">
-          <motion.figure style={{ opacity }} className="h-full">
-            <img src="/main-screen.svg" className="h-full w-auto" />
+          <motion.figure style={{ opacity, display }} className="h-full">
+            <img src="/featuer-1.svg" className="h-full w-auto" />
+          </motion.figure>
+          {/* final pic */}
+          <motion.figure style={{ opacity: finalOpacity }} className="h-full">
+            <img src="/feature-final.svg" className="h-full w-auto" />
           </motion.figure>
           <motion.figure style={{ opacity: text2Opacity }}>
             <img
-              src="/command-palette.svg"
+              src="/feature-logo.svg"
               className="absolute inset-0 h-full w-auto"
             />
           </motion.figure>
           <motion.figure style={{ opacity: text3Opacity }}>
             <img
-              src="/devtools.svg"
+              src="/feature-items.svg"
               className="absolute inset-0 h-full w-auto"
             />
           </motion.figure>
@@ -80,10 +94,10 @@ export const Features = () => {
           })}
           className="translate-y-centered-offset absolute top-1/2 left-0"
         >
-          <span className="text-primary">Preconfigured environments</span>
+          <span className="text-primary">Full-Service Brand Creation</span>
           <br />
-          We detect your environment so you don't need to fiddle with
-          configuration files.
+          We are committed to crafting your brand from scratch to a finished
+          product that reflects your values and vision.
         </motion.p>
         <motion.p
           style={stylesWithCssVar({
@@ -92,9 +106,10 @@ export const Features = () => {
           })}
           className="translate-y-centered-offset absolute top-1/2 left-0"
         >
-          <span className="text-primary">Command Pallete</span>
+          <span className="text-primary">Designing LOGO</span>
           <br />
-          Access and complete any action in seconds with the command palette.
+          We specialize in creating custom logos tailored to your unique brand
+          identity and vision.
         </motion.p>
         <motion.p
           style={stylesWithCssVar({
@@ -103,10 +118,9 @@ export const Features = () => {
           })}
           className="translate-y-centered-offset absolute top-1/2 left-0"
         >
-          <span className="text-primary">Devtools</span>
+          <span className="text-primary">content strategy</span>
           <br />
-          We've bundled useful tools to help you get your work done faster and
-          more efficiently.
+          content creation services to bring your brand to life
         </motion.p>
       </div>
     </section>
