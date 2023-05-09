7497b20daafda3e3d7ead37a1d15af9fe657a25e
jsina <jsina.maleki@gmail.com>
2023-05-09 16:42:36 +0330

    cleanup

--- src/sections/no-lockin.tsx
+++ src/sections/no-lockin.tsx
@@ -1,33 +1,18 @@
-export const  NoLockin = () => (
+import { Love } from "@/components/love";
+
+export const NoLockin = () => (
   <section className="relative z-10 h-screen bg-white py-96">
-    <h2 className="mt-12 text-center font-heading text-3xl font-bold leading-[1] tracking-tighter text-[#171618]">
+    <h2 className="mt-12 text-center font-heading text-3xl font-bold leading-[1] tracking-tighter text-secondary">
       No lock-in.
       <br />{" "}
       <span className="text-secondary">
-        Use the tools
+        Use RoqqetMedia which
         <br />
-        you love.
+        <div className="flex items-center justify-center space-x-2">
+          <span>you love</span>
+          <Love />
+        </div>
       </span>
     </h2>
-
-    <p className="mx-auto mt-20 max-w-[72rem] text-center text-xl text-black [&_a]:underline">
-      I hope you learned something by watching this rebuild, the code and the
-      video. Check out{" "}
-      <a
-        href="https://frontend.fyi/"
-        target="_blank"
-        rel="noopener nofollow noreferrer"
-      >
-        Frontend.FYI
-      </a>{" "}
-      for more videos, and definitely head over to{" "}
-      <a
-        href="https://projects.codesandbox.io"
-        rel="noopener nofollow noreferrer"
-      >
-        CodeSandbox' website
-      </a>{" "}
-      as well!
-    </p>
   </section>
 );
